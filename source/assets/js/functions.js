56ba2e3fbcc5b985d7c56a90ac6bf2d34f5035bf
Daniil Patrikeev <warptimer@gmail.com>
2017-12-13 12:21:58 +0300

    add validation

--- source/assets/js/functions.js
+++ source/assets/js/functions.js
@@ -5,4 +5,24 @@ document.addEventListener('DOMContentLoaded', function () {
   let preloader = new Preloader();
   
   preloader.off();
+
+  $('#authorization button[type="submit"]').prop('disabled', 'disabled');
+
+  $('#authorization').validate({
+    errorPlacement: function(error, element) {
+      error.insertAfter($(element).parent());
+    },
+    submitHandler: function(form) {
+      console.log(form)
+      form.submit();
+    }
+  });
+
+  $('#authorization input').on('keyup', function() {
+    if ($("#authorization").valid()) {
+      $('#authorization button[type="submit"]').prop('disabled', false);  
+    } else {
+      $('#authorization button[type="submit"]').prop('disabled', 'disabled');
+    }
+  });
 });
